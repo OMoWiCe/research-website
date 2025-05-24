f730b7a8d2d6eb67fe0c3dbbf74d55b3ade2c44d
th3-s7r4ng3r <gvinura@gmail.com>
2025-05-24 23:42:48 +0530

    Add: Research results and images

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,7 +10,7 @@ export const Home = () => {
             Occupancy Monitoring <br />
             with <span className="accent-text">Wi-Fi</span> &{" "}
             <span className="accent-text">Cellular</span>
-          </h1>
+          </h1>{" "}
           <p className="home-description">
             A Research on Occupancy Monitoring Discover how we're transforming
             real-time space awareness using passive Wi-Fi and cellular signal
@@ -20,11 +20,20 @@ export const Home = () => {
         </div>
         <div className="home-image">
           <img
-            src="https://placehold.co/600x400"
+            src="./images/preview.png"
             alt="Occupancy Monitoring"
             className="home-image-placeholder"
           />
         </div>
+      </div>
+      <div className="home-results">
+        <h3 className="results-title">Project Results</h3>
+        <p className="results-text">
+          Mean Error (MPE): <span>-14.67%</span>
+          <div className="restults-subtext">
+            Underestimate Occupancy by 14.67% on average
+          </div>
+        </p>
       </div>
     </section>
   );
